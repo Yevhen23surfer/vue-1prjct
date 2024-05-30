9ee7fb73c2365b4f54b3f109ab3863cbcd428a81
Yevhen Surfer <23skopets@gmail.com>
2024-05-30 12:21:39 +0200

    title img link

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 const app = Vue.createApp({
   data() {
-    return {
-      product: 'Socks',
-      image: './assets/images/socks_green.jpg',
-      product: 'Socks'
-    }
+      return {
+          product: 'Socks',
+          image: './assets/images/socks_green.jpg',
+          // solution
+          url: 'https://www.vuemastery.com/'
+          // solution
+      }
   }
 })
