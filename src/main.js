6a934447191ac737812b68a2046088f721ab8ee7
Yevhen Surfer <23skopets@gmail.com>
2024-05-30 13:05:00 +0200

    variants, sizes, colors

--- src/main.js
+++ src/main.js
@@ -3,9 +3,15 @@ const app = Vue.createApp({
       return {
           product: 'Socks',
           image: './assets/images/socks_green.jpg',
-          // solution
-          url: 'https://www.vuemastery.com/'
-          // solution
+          url: 'https://www.vuemastery.com/',
+          inventory: 100,
+          onSale: true,
+          details: ['50% cotton', '30% wool', '20% polyester'],
+          sizes: ['S', 'M', 'L', 'XL'],
+          variants: [
+            { id: 2234, color: 'green' },
+            { id: 2235, color: 'blue' },
+          ]
       }
   }
 })
