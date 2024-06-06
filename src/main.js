2c7461897d64e04ca6832f30b3c8d5f36fe06db0
Yevhen Surfer <23skopets@gmail.com>
2024-06-06 22:30:58 +0200

    clear

--- src/main.js
+++ src/main.js
@@ -8,7 +8,6 @@ const app = Vue.createApp({
           inventory: true,
           onSale: true,
           details: ['50% cotton', '30% wool', '20% polyester'],
-          sizes: ['S', 'M', 'L', 'XL'],
           variants: [
             { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
             { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' },
